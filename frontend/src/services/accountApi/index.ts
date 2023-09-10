7bc76bb25373df573d2551811a3dcb51c0a2343f
conghuule <you@example.com>
2023-09-11 02:45:17 +0700

    EVS-150: Fix UpdateProfile

---
+++ frontend/src/services/accountApi/index.ts
@@ -0,0 +1,20 @@
+import axiosClient from '../axiosClient'
+
+import type { ProfileParams } from './types'
+
+const PREFIX = 'account/'
+
+const accountApi = {
+  update: async (data: ProfileParams) => {
+    try{
+    const res = await axiosClient.patch(`${PREFIX}profile`, data);
+    return res;
+    } catch (error){
+        console.error("Error updating profile:", error);
+        throw error;
+    }
+  },
+
+}
+
+export default accountApi
