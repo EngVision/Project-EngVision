be0afe21a9007cb601537728be23e98d55058200
conghuule <you@example.com>
2023-09-13 20:04:39 +0700

    EVS-163: add color constant

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -13,5 +13,29 @@ module.exports = {
         sans: ['Poppins', ...defaultTheme.fontFamily.sans],
       },
     },
+    color: {
+      dark: '#101210',
+      white: '#FFFFFF',
+      grey: {
+        100: '#F5F6F8',
+        200: '#E6E9EF',
+        300: '#C5C7D0',
+      },
+      primary: {
+        100: '#CCE5FF',
+        200: '#0073EA',
+        300: '#0060B9',
+      },
+      positive: {
+        100: '#BBDBC9',
+        200: '#258750',
+        300: '#007038',
+      },
+      negative: {
+        100: '#F4C3CB',
+        200: '#D83A52',
+        300: '#B63546',
+      },
+    },
   },
 }
