a825fd12856c1813592bcd44870794e33eb26846
conghuule <you@example.com>
2023-09-28 00:14:29 +0700

    EVS-108: fix layout as VuNguyen's feedback

--- frontend/src/layouts/DefaultLayout.tsx
+++ frontend/src/layouts/DefaultLayout.tsx
@@ -9,9 +9,11 @@ const DefaultLayout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="flex flex-row h-[100vh]">
       <Sidebar />
-      <div className="flex flex-1">
-        <div className="px-8 flex-1 flex-col bg-bgNeutral">
+      <div className="flex flex-1 flex-col">
+        <div className="px-8  bg-bgNeutral">
           <Header />
+        </div>
+        <div className="px-8 flex-1  overflow-y-scroll bg-bgNeutral">
           {children}
         </div>
       </div>
