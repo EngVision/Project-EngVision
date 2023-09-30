0dfa921fb97c5f18a309466ff8e46babb9be5ab1
Duy Tan <duytan030522@gmail.com>
2023-09-30 17:19:32 +0700

    EVS-149: use notification antd instead toastify

--- frontend/src/pages/SendMailResetPassword/index.tsx
+++ frontend/src/pages/SendMailResetPassword/index.tsx
@@ -1,14 +1,14 @@
-import { Button, Form, Input } from 'antd'
+import { Button, Form, Input, notification } from 'antd'
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { Email } from '../../services/accountApi/types'
 import accountApi from '../../services/accountApi'
-import { toast } from 'react-toastify'
 import { ROUTES } from '../../utils/constants'
 
 const SendMailResetPassword: React.FC = () => {
   const [error, setError] = useState<string>('')
   const [sentMail, setSentMail] = useState<boolean>(false)
+  const [apiNotification, contextHolder] = notification.useNotification()
   const navigate = useNavigate()
 
   const validateEmail = (email: string) => {
@@ -21,8 +21,15 @@ const SendMailResetPassword: React.FC = () => {
     try {
       const rs = await accountApi.sendMailForgotPassword(values)
       if (rs.data.success) {
-        toast.success(rs.data.message)
-      } else toast.error('Send mail failed')
+        apiNotification.success({
+          message: 'Success',
+          description: rs.data.message,
+        })
+      } else
+        apiNotification.error({
+          message: 'Error',
+          description: rs.data.message,
+        })
     } catch (error) {
       setSentMail(false)
       setError(error.response.data.message)
@@ -30,83 +37,87 @@ const SendMailResetPassword: React.FC = () => {
   }
 
   return (
-    <div className="w-[515px] bg-white px-[32px] py-[24px] rounded-[16px] w">
-      <div>
-        <h4 className="text-center text-[40px]">Forgot Your Password?</h4>
-        <p className=" text-[#9D9DAD] my-[20px] text-lg">
-          Just enter your email and we’ll send you a link to reset your password
-        </p>
-      </div>
+    <>
+      {contextHolder}
+      <div className="w-[515px] bg-white px-[32px] py-[24px] rounded-[16px] w">
+        <div>
+          <h4 className="text-center text-[40px]">Forgot Your Password?</h4>
+          <p className=" text-[#9D9DAD] my-[20px] text-lg">
+            Just enter your email and we’ll send you a link to reset your
+            password
+          </p>
+        </div>
 
-      <div>
-        <Form
-          name="basic"
-          style={{ maxWidth: 600 }}
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-          autoComplete="off"
-        >
-          <Form.Item
-            name="email"
-            className="mb-[0px]"
-            rules={[
-              {
-                async validator(_, value) {
-                  return new Promise((resolve, reject) => {
-                    if (validateEmail(value)) {
-                      resolve('')
-                    } else reject(new Error('Invalid email'))
-                  })
+        <div>
+          <Form
+            name="basic"
+            style={{ maxWidth: 600 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            autoComplete="off"
+          >
+            <Form.Item
+              name="email"
+              className="mb-[0px]"
+              rules={[
+                {
+                  async validator(_, value) {
+                    return new Promise((resolve, reject) => {
+                      if (validateEmail(value)) {
+                        resolve('')
+                      } else reject(new Error('Invalid email'))
+                    })
+                  },
                 },
-              },
-            ]}
-          >
-            <Input
-              placeholder="Email"
-              size="middle"
-              className="rounded-[8px] h-[44px]"
-              onChange={() => setError('')}
-            />
-          </Form.Item>
-
-          {error && <p className="text-[#ff4d4f] mt-[3px]">{error}</p>}
-
-          <Form.Item className="text-center">
-            <Button
-              type="primary"
-              shape="round"
-              htmlType="submit"
-              disabled={sentMail}
-              className="w-full font-semibold h-[44px] text-xl mt-[20px]"
+              ]}
             >
-              Reset Password
-            </Button>
-          </Form.Item>
+              <Input
+                placeholder="Email"
+                size="middle"
+                className="rounded-[8px] h-[44px]"
+                onChange={() => setError('')}
+              />
+            </Form.Item>
 
-          <p
-            className="text-[#0073EA] text-xl font-semibold text-center cursor-pointer my-[28px]"
-            onClick={() => navigate(ROUTES.signIn)}
-            role="presentation"
-          >
-            Return to Sign In?
-          </p>
+            {error && <p className="text-[#ff4d4f] mt-[3px]">{error}</p>}
 
-          {sentMail && (
-            <p className="text-center text-[#9D9DAD] text-xl mb-[30px]">
-              Didn’t receive any email? Try looking up in your Spams or try
-              again after
+            <Form.Item className="text-center">
+              <Button
+                type="primary"
+                shape="round"
+                htmlType="submit"
+                disabled={sentMail}
+                className="w-full font-semibold h-[44px] text-xl mt-[20px]"
+              >
+                Reset Password
+              </Button>
+            </Form.Item>
+
+            <p
+              className="text-[#0073EA] text-xl font-semibold text-center cursor-pointer my-[28px]"
+              onClick={() => navigate(ROUTES.signIn)}
+              role="presentation"
+            >
+              Return to Sign In?
             </p>
-          )}
 
-          <p className="text-center text-[#9D9DAD] text-xl">
-            Having troubles?
-            <Link to="/sign-up" className="font-semibold text-[#0073EA] pl-2">
-              Contact Us
-            </Link>
-          </p>
-        </Form>
+            {sentMail && (
+              <p className="text-center text-[#9D9DAD] text-xl mb-[30px]">
+                Didn’t receive any email? Try looking up in your Spams or try
+                again after
+              </p>
+            )}
+
+            <p className="text-center text-[#9D9DAD] text-xl">
+              Having troubles?
+              <Link to="/sign-up" className="font-semibold text-[#0073EA] pl-2">
+                Contact Us
+              </Link>
+            </p>
+          </Form>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
