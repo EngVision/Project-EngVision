7da521e9f9bab3ad89b20b73e86b0855658eb2ea
Duy Tan <duytan030522@gmail.com>
2023-10-03 11:39:54 +0700

    EVS-159: add update sections in update course

--- backend/src/modules/courses/dto/update-course.dto.ts
+++ backend/src/modules/courses/dto/update-course.dto.ts
@@ -1,4 +1,10 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiPropertyOptional, PartialType } from '@nestjs/swagger';
 import { CreateCourseDto } from './create-course.dto';
+import { SectionDto } from './section.dto';
+import { IsOptional } from 'class-validator';
 
-export class UpdateCourseDto extends PartialType(CreateCourseDto) {}
+export class UpdateCourseDto extends PartialType(CreateCourseDto) {
+  @IsOptional()
+  @ApiPropertyOptional({ type: [SectionDto], description: 'sections' })
+  sections?: SectionDto[];
+}
