d30046cc94b514703c7433e8056fa9841ed69eaf
Kiet Le <letankietpdcd@gmail.com>
2023-10-18 20:03:10 +0700

    EVS-224: Fix bug of create account when sign in with vendor

--- frontend/src/pages/SignUp/TeacherSignUp.tsx
+++ frontend/src/pages/SignUp/TeacherSignUp.tsx
@@ -22,6 +22,7 @@ const TeacherSignUp: React.FC = () => {
   const navigate = useNavigate()
   const dispatch = useAppDispatch()
   const apiNotification = useContext(NotificationContext)
+  const [form] = Form.useForm<SignUpParams>()
 
   const [error, setError] = useState<string>('')
 
@@ -106,6 +107,30 @@ const TeacherSignUp: React.FC = () => {
     },
   ]
 
+  const validateEmail = (email: string) => {
+    if (!email) return true
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+    return emailRegex.test(email)
+  }
+
+  const validatePassword = (password: string) => {
+    if (!password) return true
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\W_]).{8,}$/
+    return password && password.length >= 8 && passwordRegex.test(password)
+  }
+
+  const validateConfirmPassword = (confirmPassword: string) => {
+    if (!confirmPassword) return true
+    const password = form.getFieldValue('password')
+    return confirmPassword === password
+  }
+
+  const validatePhone = (phone: string) => {
+    if (!phone) return true
+    const phoneRegex = /^\d{10,}$/
+    return phoneRegex.test(phone)
+  }
+
   return (
     <div className="flex flex-col bg-bgNeutral p-8 rounded-[16px] gap-6">
       <div className="flex flex-col items-center">
@@ -123,6 +148,7 @@ const TeacherSignUp: React.FC = () => {
         onChange={() => setError('')}
         className="w-[560px] flex flex-col"
         layout="vertical"
+        form={form}
       >
         <div className="flex gap-4">
           <Form.Item<SignUpParams>
@@ -159,7 +185,18 @@ const TeacherSignUp: React.FC = () => {
         <Form.Item<SignUpParams>
           name="email"
           label="Email"
-          rules={[{ message: 'Please input your email!', required: true }]}
+          rules={[
+            { message: 'Please input your email!', required: true },
+            {
+              async validator(_, value) {
+                return new Promise((resolve, reject) => {
+                  if (validateEmail(value)) {
+                    resolve('')
+                  } else reject(new Error('Invalid email'))
+                })
+              },
+            },
+          ]}
         >
           <Input
             placeholder="Email"
@@ -185,7 +222,23 @@ const TeacherSignUp: React.FC = () => {
         <Form.Item<SignUpParams>
           name="password"
           label="Password"
-          rules={[{ message: 'Please input your password!', required: true }]}
+          rules={[
+            { message: 'Please input your password!', required: true },
+            {
+              async validator(_, value) {
+                return new Promise((resolve, reject) => {
+                  if (validatePassword(value)) {
+                    resolve('')
+                  } else
+                    reject(
+                      new Error(
+                        'The password must be at least 8 characters long and contain at least 1 uppercase letter, 1 lowercase letter, and 1 number or special character',
+                      ),
+                    )
+                })
+              },
+            },
+          ]}
         >
           <Input.Password
             placeholder="Password"
@@ -202,6 +255,20 @@ const TeacherSignUp: React.FC = () => {
               message: 'Please input your confirm password!',
               required: true,
             },
+            {
+              async validator(_, value) {
+                return new Promise((resolve, reject) => {
+                  if (validateConfirmPassword(value)) {
+                    resolve('')
+                  } else
+                    reject(
+                      new Error(
+                        'The confirm password must be same as password!',
+                      ),
+                    )
+                })
+              },
+            },
           ]}
         >
           <Input.Password
@@ -216,6 +283,20 @@ const TeacherSignUp: React.FC = () => {
           label="Phone Number"
           rules={[
             { message: 'Please input your phone number!', required: true },
+            {
+              async validator(_, value) {
+                return new Promise((resolve, reject) => {
+                  if (validatePhone(value)) {
+                    resolve('')
+                  } else
+                    reject(
+                      new Error(
+                        'Phone must be longer than or equal to 10 characters',
+                      ),
+                    )
+                })
+              },
+            },
           ]}
         >
           <Input placeholder="Phone Number" className="rounded-lg px-3 py-2" />
