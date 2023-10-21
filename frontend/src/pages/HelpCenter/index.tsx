14ad14a3b11afba743a339faf60acf16bad0fee3
conghuule <you@example.com>
2023-10-21 15:46:39 +0700

    EVS-193 fix: remove useless code

--- frontend/src/pages/HelpCenter/index.tsx
+++ frontend/src/pages/HelpCenter/index.tsx
@@ -1,7 +1,4 @@
-import React from 'react'
-
 import LogoutButton from '../../components/Header/LogoutButton'
-import LocalesButton from '../../components/Header/LocalesButton'
 
 const HelpCenter = () => {
   return (
