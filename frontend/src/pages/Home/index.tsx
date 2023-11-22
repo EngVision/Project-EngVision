37a187e1e380fcbed36580c407e09ed7441d8483
minhtringuyennn <contact@minhtringuyennn.com>
2023-11-22 21:25:32 +0700

    fix: dashboard bug

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -37,7 +37,7 @@ const Home = () => {
       totalInProcess: 0,
     }
 
-    if (rawSubmissionList) {
+    if (rawSubmissionList?.data) {
       rawSubmissionList.data.forEach((assignment) => {
         if (assignment.totalDone) {
           exercise.totalDone += assignment.totalDone
