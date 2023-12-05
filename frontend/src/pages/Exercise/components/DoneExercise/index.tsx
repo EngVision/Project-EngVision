8acd97febd9e2c6a94895ac071f98b3a69f6a68f
Vu Nguyen <longvu2907@gmail.com>
2023-12-05 20:22:26 +0700

    display user score and level when done exercise

--- frontend/src/pages/Exercise/components/DoneExercise/index.tsx
+++ frontend/src/pages/Exercise/components/DoneExercise/index.tsx
@@ -1,19 +1,45 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useAppDispatch, useAppSelector } from '../../../../hooks/redux'
+import userLevelApi from '../../../../services/userLevelApi'
+import { setCurrentLevel } from '../../../../redux/app/slice'
+import { Progress } from 'antd'
 
 interface DoneExerciseProps {
   grade?: number
 }
 
 function DoneExercise({ grade }: DoneExerciseProps) {
+  const dispatch = useAppDispatch()
+  const currentLevel = useAppSelector((state) => state.app.currentLevel)
+
+  useEffect(() => {
+    const getUserLevel = async () => {
+      const level = await userLevelApi.getUserLevel()
+      dispatch(setCurrentLevel(level))
+    }
+
+    getUserLevel()
+  }, [])
+
   return (
     <div className="h-full flex flex-col gap-4 justify-center text-center text-primary">
       <p className="text-2xl font-semibold">
         Your test result has completed with
       </p>
-      <p className="text-4xl font-bold">{grade?.toFixed(2) || 0} / 10</p>
-      <p className="text-xl font-medium">
-        You just ranking up to <span className="font-bold">B2</span>
-      </p>
+      <p className="text-4xl font-bold">{grade?.toFixed(2) || 0} / 10.00</p>
+      <Progress
+        type="circle"
+        percent={((currentLevel?.overall || 0) / 250) * 100}
+        size={200}
+        format={() => (
+          <div className="flex flex-col gap-2">
+            <p className="text-2lg">{currentLevel?.overall}</p>
+            <p className="text-2lg">
+              Level <span className="font-bold">{currentLevel?.CEFRLevel}</span>
+            </p>
+          </div>
+        )}
+      />
     </div>
   )
 }
