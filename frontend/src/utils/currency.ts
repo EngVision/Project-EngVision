e20b65b8c0e980f7c96741a778399a6a8ae5850c
conghuule <leconghuu.lch@gmail.com>
2024-03-03 16:02:30 +0700

    fix format currency

--- frontend/src/utils/currency.ts
+++ frontend/src/utils/currency.ts
@@ -1,5 +1,5 @@
 export const formatCurrency = (value: number) => {
-  return new Intl.NumberFormat('en-US', {
+  return new Intl.NumberFormat('vi-VN', {
     style: 'currency',
     currency: 'VND',
   }).format(value)
