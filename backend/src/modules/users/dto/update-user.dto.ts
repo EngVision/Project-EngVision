d3e14f1acaa50c3fe9f61fbe18e5706b728ef04e
Duy Tan <tantd@algolabs.vn>
2024-03-07 00:30:17 +0700

    EVS-374: add update bank info, fix bugs

--- backend/src/modules/users/dto/update-user.dto.ts
+++ backend/src/modules/users/dto/update-user.dto.ts
@@ -1,6 +1,17 @@
-import { OmitType, PartialType } from '@nestjs/swagger';
+import { ApiProperty, OmitType, PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
+import { IsOptional, IsString } from 'class-validator';
 
 export class UpdateUserDto extends PartialType(
   OmitType(CreateUserDto, ['password']),
-) {}
+) {
+  @IsOptional()
+  @IsString()
+  @ApiProperty({ type: String, description: 'Bank name' })
+  bankName?: string;
+
+  @IsOptional()
+  @IsString()
+  @ApiProperty({ type: String, description: 'Bank account number' })
+  bankNumber?: string;
+}
